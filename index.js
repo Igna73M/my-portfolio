6b46386903c084ad8c88178c8de016c293ad6489
MUGABE Nshuti Ignace <ignacemugabe@gmail.com>
2025-08-29 21:29:02 +0200

    Portfolio V1.0.4

--- index.js
+++ index.js
@@ -37,3 +37,26 @@ form.addEventListener("submit", function (e) {
       }, 3000);
     });
 });
+
+// Scrollers Animation
+
+const scrollers = document.querySelectorAll(".scroller");
+
+if (!window.matchMedia("(prefers.reduced-motion:reduce)").matches) {
+  addAnimation();
+}
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    scroller.setAttribute("data-animated", true);
+
+    const scrollerInner = scroller.querySelector(".scroller-inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    scrollerContent.forEach((item) => {
+      const duplicateItem = item.cloneNode(true);
+      duplicateItem.setAttribute("aria-hidden", "true");
+      scrollerInner.appendChild(duplicateItem);
+    });
+  });
+}
